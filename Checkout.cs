265a18d082cf01c21f8ae478d5334b591c7fc147
MJPCrepin <manuel@crep.in>
2017-07-12 13:45:17 +0100

    First commit

---
+++ Checkout.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Checkout
+{
+
+    class Checkout
+    {
+        // Set of legal items for sale
+        public Dictionary<string, double> inventory = new Dictionary<string, double>();
+
+        // Set of current items checked with quantity for each item
+        private Dictionary<string, int> cart = new Dictionary<string, int>();
+
+        public double returnTotal(string[] list)
+        {
+            parseList(list);
+            printReceipt();
+            return calculateTotal();
+        }
+
+        private void parseList(string[] list)
+        {
+            foreach (var token in list)
+            {
+                // Normalise tokens (lowercase)
+                var x = token.ToLower();
+
+                // If legal item, add to cart, else warn & continue
+
+                if (inventory.ContainsKey(x))
+                {
+                    if (cart.ContainsKey(x))
+                    {
+                        cart[x]++;
+                    }
+                    else
+                    {
+                        cart.Add(x, 1);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid item: " + x + " isn't for sale!");
+                }
+
+            }
+        }
+
+        private double calculateTotal()
+        {
+            // For each item multiply cart counter with inventory price
+            double total = 0;
+            foreach (var x in cart)
+            {
+                total += x.Value * inventory[x.Key];
+            }
+            return total;
+        }
+
+        private void printReceipt()
+        {
+            calculateTotal();
+            foreach (var x in cart) Console.WriteLine(x.Value+"x "+x.Key+" @ "+ inventory[x.Key].ToString() + " GBP");
+            Console.WriteLine("Total: " + calculateTotal().ToString()+ " GBP");
+        }
+
+        public void reset()
+        {
+            cart.Clear();
+            Console.WriteLine("\n[Checkout reset]");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+
+namespace Checkout
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            // Instantiate till and set legal items (could also be done from ext src)
+            Checkout till = new Checkout();
+            till.inventory.Add("apple", 0.6);
+            till.inventory.Add("orange", 0.25);
+
+            // Start tests
+            initiateTests(till);
+
+            // Normal operation mode
+            Console.WriteLine("\nThank you for using SimpleCheckout, here is your receipt:");
+            till.returnTotal(args);
+
+            // Prevent Exit
+            Console.ReadLine();
+        }
+
+        private static void initiateTests(Checkout till)
+        {
+            int testNumber = 1;
+
+            Console.WriteLine("Initiating system tests: \n");
+
+            // Check if legal items are set
+            Console.WriteLine("\n"+testNumber + ": Check if legal items are set" + "\n");
+            try
+            {
+                Console.WriteLine("Items currently for sale:");
+                foreach (var x in till.inventory) Console.WriteLine(x.Key + ": " + x.Value + " GBP");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nExpected result:");
+                Console.WriteLine("apple: 0.6 GBP");
+                Console.WriteLine("orange: 0.25 GBP");
+
+                testNumber++;
+                till.reset();
+                Console.WriteLine("-----------------------------------------------------");
+            }
+
+
+            // Check if you can add an apple from a list
+            Console.WriteLine("\n" + testNumber + ": Check if you can add an apple from a list" + "\n");
+            try
+            {
+                string[] args = { "apple" };
+                till.returnTotal(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nExpected result:");
+                Console.WriteLine("1x apple @ 0.6 GBP");
+                Console.WriteLine("Total: 0.6 GBP");
+
+                testNumber++;
+                till.reset();
+                Console.WriteLine("-----------------------------------------------------");
+            }
+
+            // Check if you can add an orange from a list
+            Console.WriteLine("\n" + testNumber + ": Check if you can add an orange from a list" + "\n");
+            try
+            {
+                string[] args = { "orange" };
+                till.returnTotal(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nExpected result:");
+                Console.WriteLine("1x orange @ 0.25 GBP");
+                Console.WriteLine("Total: 0.25 GBP");
+
+                testNumber++;
+                till.reset();
+                Console.WriteLine("-----------------------------------------------------");
+            }
+
+            // Add 3 apples and 3 oranges
+            Console.WriteLine("\n" + testNumber + ": Add 3 apples and 3 oranges" + "\n");
+            try
+            {
+                string[] args = { "apple","apple","apple","orange","orange","orange" };
+                till.returnTotal(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nExpected result:");
+                Console.WriteLine("3x apple @ 0.6 GBP");
+                Console.WriteLine("3x orange @ 0.25 GBP");
+                Console.WriteLine("Total: 2.55 GBP");
+
+                testNumber++;
+                Console.WriteLine("-----------------------------------------------------");
+            }
+
+            // Add 1 more of each
+            Console.WriteLine("\n" + testNumber + ": Add 1 more of each" + "\n");
+            try
+            {
+                string[] args = { "apple", "orange" };
+                till.returnTotal(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nExpected result:");
+                Console.WriteLine("4x apple @ 0.6 GBP");
+                Console.WriteLine("4x orange @ 0.25 GBP");
+                Console.WriteLine("Total: 3.4 GBP");
+
+                testNumber++;
+                till.reset();
+                Console.WriteLine("-----------------------------------------------------");
+            }
+
+            // Try buying illegal item (avocado)
+            Console.WriteLine("\n" + testNumber + ": Try buying illegal item" + "\n");
+            try
+            {
+                string[] args = { "avocado" };
+                till.returnTotal(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nExpected result:");
+                Console.WriteLine("Invalid item: avocado isn't for sale!");
+                Console.WriteLine("Total: 0 GBP");
+
+                testNumber++;
+                till.reset();
+                Console.WriteLine("-----------------------------------------------------");
+            }
+
+            // Try buying legal item in non normalised form (ALL CAPS) (eg ORANGE)
+            Console.WriteLine("\n" + testNumber + ": Try buying legal item in non normalised form (ALL CAPS)" + "\n");
+            try
+            {
+                string[] args = { "ORANGE" };
+                till.returnTotal(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nExpected result:");
+                Console.WriteLine("1x orange @ 0.25 GBP");
+                Console.WriteLine("Total: 0.25 GBP");
+
+                testNumber++;
+                till.reset();
+                Console.WriteLine("-----------------------------------------------------");
+            }
+
+            // Try buying from empty list
+            Console.WriteLine("\n" + testNumber + ": Try buying from empty list" + "\n");
+            try
+            {
+                string[] args = {};
+                till.returnTotal(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nExpected result:");
+                Console.WriteLine("Total: 0 GBP");
+
+                testNumber++;
+                till.reset();
+                Console.WriteLine("-----------------------------------------------------");
+            }
+
+        }
+
+
+        
+    }
+}
